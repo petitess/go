baf59e55ae57db949acf48d42a4a5e4167a88abf
Karol <93782725+petitess@users.noreply.github.com>
2025-03-10 14:40:05 +0100

    arrays, slices

--- xxx01/array01.go
+++ xxx01/array01.go
@@ -12,7 +12,8 @@ func main() {
 	var productNames [4]string
 	productNames = [4]string{"Apple", "Banana", "Orange", "Grapes"}
 	productNames[2] = "Mango"
-	prices := [4]float64{100.0, 200.0, 300.0, 400.0}
+	prices := []float64{100.0, 200.0, 300.0, 400.0}
+	prices = append(prices, 500.0)
 	fmt.Println(prices)
 	fmt.Println(productNames)
 
